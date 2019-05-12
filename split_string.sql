2e6532021bd1c3470572a89523b2bb486f6c25b9
travis88 <hellsballs99@gmail.com>
2019-05-12 20:14:42 +0300

    left

--- split_string.sql
+++ split_string.sql
@@ -1,10 +1,12 @@
-CREATE OR REPLACE FUNCTION common.split_string(str character varying, _length integer)
+CREATE OR REPLACE FUNCTION common.split_string(str character varying, _length integer, tr boolean DEFAULT true)
  RETURNS character varying
  LANGUAGE plpgsql
 AS $function$
 DECLARE res CHARACTER VARYING; 
 BEGIN
-	str := trim(str);
+	IF tr = TRUE THEN
+		str := trim(str);
+    END IF;
 
 	SELECT LEFT(LEFT(str, _length),
 			_length - POSITION(' ' IN reverse(LEFT(str, _length))))
--- to_left.sql
+++ to_left.sql
@@ -1,10 +1,32 @@
-CREATE OR REPLACE FUNCTION common.to_left(str character varying, max_length integer)
+CREATE OR REPLACE FUNCTION common.to_left(prefix character varying, str character varying, max_length integer)
  RETURNS character varying
  LANGUAGE plpgsql
 AS $function$
-DECLARE res CHARACTER VARYING;
+DECLARE res CHARACTER VARYING := ''; -- результат
+		pref_len integer := length(prefix); -- длина префикса
+		str_len integer := length(str); -- длина строки
+		_str CHARACTER VARYING := prefix || str; -- редактируемая строка
+		_res_counter CHARACTER VARYING := ''; -- конкатенируемая строка для проверки
+		_counter integer := 0; -- счётчик
 BEGIN
-	RETURN rpad(COALESCE(str, ' '), max_length);
+	IF pref_len + str_len > max_length THEN
+		LOOP 
+			_str := (SELECT common.split_string(_str, max_length - pref_len, FALSE));
+			_res_counter := _res_counter || _str;
+			res := res || _str || chr(10);
+			
+			IF length(_res_counter) >= pref_len + str_len THEN
+				EXIT;
+			END IF;
+			_counter := _counter + length(_str);
+			_str := ltrim(RIGHT(prefix || str, length(prefix || str) - length(_res_counter)));
+--			_str := ltrim(RIGHT(str, length(str) - length(_res_counter) - pref_len));
+			_str := rpad(lpad(_str, pref_len + length(_str), ' '), max_length, ' ');
+		END LOOP;
+	ELSE 
+		res := rpad(COALESCE(prefix || str, ' '), max_length);
+	END IF;
+	RETURN res;
 END;
 $function$
 ;
